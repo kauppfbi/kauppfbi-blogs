e1bc295c877a574b4423fa9603d84b1127cb42ec
Fabian K <fabian.kaupp@mail.de>
2021-11-06 09:55:26 +0100

    feat: add secondary entrypoint for greeter library

--- libs/shared/greeter/src/index.ts
+++ libs/shared/greeter/src/index.ts
@@ -1,6 +1,2 @@
 export * from './lib/simple-greeter/simple-greeter.module';
 export * from './lib/simple-greeter/simple-greeter.component';
-
-export * from './lib/fancy-greeter/fancy-greeter.module';
-export * from './lib/fancy-greeter/fancy-greeter.component';
-
